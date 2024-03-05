395760e6e0614de5c5afce48ad4e0a203b958a3e
TylorDev <jhontmg26@gmail.com>
2024-03-04 23:30:25 -0300

    image created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div
+      className="flex items-center justify-center min-h-screen p-4 bg-center bg-cover "
+      style={{ backgroundImage: "url('bg2.gif')" }}
+    >
+      {/* <button className="text-3xl"> X </button> */}
 
-export default App
+      <div className="[main] flex  flex-col h-full rounded-xl  bg-[#0E0D0D] ">
+        <div
+          className="[Left] flex  justify-around h-40 bg-cover relative"
+          style={{ backgroundImage: "url('back2.png')" }}
+        >
+          <div className="[Left-Text]  relative left-0 text-center text-white">
+            <div className="text-xl font-bold "> HI THERE!</div>
+            <div> Join us and enjoy! :) </div>
+          </div>
+          <img
+            className="absolute  h-full  scale-[120%]  bottom-4 "
+            src="main2.webp"
+            alt="XD"
+          />
+        </div>
+
+        <div className="[Right]     border-4">
+          <form action="login">
+            <label htmlFor="email">Email</label>
+            <input name="email" type="text" />
+            <label htmlFor="username">Password</label>
+            <input type="password" name="password" />
+            <input type="checkbox" className="text-indigo-600 form-checkbox" />
+            <span className="ml-2">
+              Recordarme cuando vuelva a iniciar sesion
+            </span>
+            <input type="checkbox" className="text-indigo-600 form-checkbox" />
+            <span className="ml-2">Acepto los términos y condiciones</span>
+            <button type="submit"> REGISTRATION!</button>
+          </form>
+          <div className="[Social]">
+            <div> AC </div>
+            <div> AC </div>
+            <div> AC </div>
+            <div> AC </div>
+            <div> AC </div>
+          </div>
+          <div>
+            {" "}
+            Have an acount? <a href="/">Login</a>
+          </div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit illum
+          temporibus aut, soluta, blanditiis nisi maiores ut qui consectetur hic
+          eius optio possimus commodi iure eaque! Explicabo ea molestiae
+          pariatur! lore
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
